2011e5b697ace0ff47f7168eb728db3bd6ed6b96
Richard Widtmann <rwidtmann@sbcglobal.net>
2022-04-26 15:37:52 -0500

    End of 22.2.8

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,11 +7,13 @@ const { Provider } = StoreContext;
 
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: ''
+  });
     // use this to confirm it works!
     console.log(state);
     return <Provider value={[state, dispatch]} {...props} />;
